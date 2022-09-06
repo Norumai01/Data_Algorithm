152926c13acae4b4ad0a13734e628b20adad0e89
Norumai01 <johnnydinhnguyen2001@gmail.com>
2022-09-06 08:58:51 -0400

    Recursion Review

---
+++ Recursive_Factorial/recursive.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int recursiveFactorial(int n) {
+    if (n == 0) return 1;
+    else n * recursiveFactorial(n-1);
+}
+
+int iterativeFactorial(int n) {
+    int prod = 1;
+    for (int i = n; i >= 1; i--) {
+        prod = prod * i;
+    }
+    return prod;
+}
+
+int main() {
+    int val = 5;
+    
+    // Testing recursive Factorial
+    std::cout << "Recursive: " << recursiveFactorial(val) << std::endl;
+    // Testing iterative Factorial
+    std::cout << "Iterative: " << iterativeFactorial(val) << std::endl;
+    return EXIT_SUCCESS;
+}
