b3c7f3d9d6c4a42dfa17963d32cfcccefe0b8945
Norumai01 <johnnydinhnguyen2001@gmail.com>
2022-10-13 19:53:00 -0400

    Testing binary tree

---
+++ Binary_Tree_Test/treeTest.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+
+
+struct TreeNode {
+	int ele;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode *parent;
+	TreeNode() : ele{0}, left{nullptr}, right{nullptr}, parent{nullptr} {}
+	TreeNode(int x) : ele{x}, left{nullptr}, right{nullptr}, parent{nullptr} {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : ele{x}, left{left}, right{right}, parent{nullptr} {}
+};
+
+
+int treeHeight(TreeNode *p) {
+	// Your code here
+	if (p == nullptr) {
+		return 0;
+	}
+	else {
+		int leftHei = treeHeight(p->left);
+		int rightHei = treeHeight(p->right);
+		return std::max(leftHei,rightHei) + 1;
+	}
+}
+
+void printInOrder(TreeNode *p) {
+    if (p == nullptr) {
+        return;
+    }
+    printInOrder(p->left);
+    std::cout << p->ele << " ";
+    printInOrder(p->right);
+}
+
+
+// Test
+int main() {
+	// Test 1
+	TreeNode t1(15); // leaf
+	TreeNode t2(7); // leaf
+	TreeNode t3(9); // leaf
+	TreeNode t5(20, &t1, &t2); // internal node
+	TreeNode t6(3, &t3, nullptr); // internal node
+	TreeNode t7(1, &t5, &t6); // root
+
+	std::cout << "Tree Height is: " << treeHeight(&t7) << std::endl; // 3
+	std::cout << "InOrder traversal: ";
+	printInOrder(&t7);
+	std::cout << std::endl;
+
+	// Test 2
+
+	TreeNode n4(4, nullptr, nullptr); // leaf
+	TreeNode n3(3, nullptr, &n4); // internal node
+	TreeNode n2(2, &n3, nullptr); // internal node
+	TreeNode n1(1, nullptr, &n2); // root
+
+	std::cout << "Tree Height is: " << treeHeight(&n1) << std::endl; // 4
+    std::cout << "InOrder traversal: ";
+    printInOrder(&n1);
+    std::cout << std::endl;
+}
