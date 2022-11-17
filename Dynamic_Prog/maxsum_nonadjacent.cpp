7c785981727233739e9d898622aa7d3ebec0ca46
Norumai01 <johnnydinhnguyen2001@gmail.com>
2022-11-17 15:59:49 -0500

    Finish HW #12

--- Dynamic_Prog/maxsum_nonadjacent.cpp
+++ Dynamic_Prog/maxsum_nonadjacent.cpp
@@ -14,10 +14,19 @@ using std::vector;
 
 int maxSum(vector<int>& nums) {
 // Your code here
+	// Include current max at starting element.
+	int incl = nums[0];
+	int excl = 0;
+	int excl_new = 0;
 
-
-
-
+	for (int i = 1; i < nums.size(); i++) {
+		// Current max excludes i.
+		excl_new = max(incl, excl);
+		
+		incl = excl + nums[i];
+		excl = excl_new;
+	}
+	return max(incl,excl);
 }
 
 
